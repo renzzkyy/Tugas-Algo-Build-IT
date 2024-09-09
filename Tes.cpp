70b81d602884e21398c05435fc06c55e109fd9bc
Darren <sebastiancorbelio@gmail.com>
2024-09-09 20:12:27 +0800

    first commit

---
+++ Tes.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Darren Sebastian"<<endl;
+    return 0;
+}
---
+++ ganjil genap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int nilai = 9;
+    if (nilai %2 != 0) {printf ("genap");
+    } else {printf ("ganjil");
+    }
+    return 0;
+}
